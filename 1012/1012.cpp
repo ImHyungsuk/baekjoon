fa7a8cbe55c09bdd4f4b4c4cd2af11368a6bb30c
LimHyeongseok <limhs8630@gmail.com>
2023-11-08 13:16:58 +0900

    Add annotation

--- 1012/1012.cpp
+++ 1012/1012.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 void Find(int (*G)[50], int x, int y, int M, int N)
 {
+    // x좌표는 0이상 N미만, y좌표는 0이상 N 미만이어야 함
     if (x >= 0 && x < M && y >= 0 && y < N)
     {
+        // 재귀함수를 이용해서 4개 방향을 하나씩 탐색함
         if (G[x][y] == 1)
         {
+            // 한번 탐색한 곳은 cnt에 반영되므로 0으로 바꿈
             G[x][y] = 0;
+            // 4개 방향 탐색하여 인접한 좌표는 모두 0으로 바꿈
             Find(G, x + 1, y, M, N);
             Find(G, x, y + 1, M, N);
             Find(G, x - 1, y, M, N);
@@ -29,6 +33,7 @@ int main()
     cin >> T;
     for (int i = 0; i < T; i++)
     {
+        // 배추밭 초기화
         for (int j = 0; j < 50; j++)
         {
             for (int k = 0; k < 50; k++)
@@ -36,13 +41,18 @@ int main()
                 G[j][k] = 0;
             }
         }
+        // 배추 입력
         cin >> M >> N >> K;
         for (int j = 0; j < K; j++)
         {
             cin >> X >> Y;
             G[X][Y] = 1;
         }
+        // cnt는 지렁이 수
         cnt = 0;
+        // Find 재귀함수로 사용
+        // 배추밭 (0,0)부터 차례대로 탐색
+        // 만약 배추가 심어져 있으면 cnt++ 후 Find함수
         for (int j = 0; j < M; j++)
         {
             for (int k = 0; k < N; k++)
