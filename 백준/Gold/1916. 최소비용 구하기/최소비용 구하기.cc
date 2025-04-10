afca5e49190e50497dbcbdb199b2ca50420f5136
ImHyungsuk <105403103+ImHyungsuk@users.noreply.github.com>
2025-04-10 16:11:37 +0900

    [Gold V] Title: 최소비용 구하기, Time: 32 ms, Memory: 4744 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -1,54 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <cstring>
+#include<bits/stdc++.h>
 using namespace std;
-int vis[1003]; // 각 도시까지 비용 
-vector<pair<int, int>> v[1003]; // <도착도시, 비용>
-void fc(int a){
-    priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int, int>>> pq;
-    // 우선순위 큐 오름차순 정렬
-    // <비용, 도착 도시>
-    pq.push(make_pair(0, a));  
-    vis[a] = 0; // 출발 도시 비용 = 0
-    while (!pq.empty())
-    {
-        int cost = pq.top().first; // 현재 도시까지 비용
-        int x = pq.top().second; // 현재 도시
+vector<pair<int,int>>v[1001];
+int N,M,s,e;
+int dist[1001];
+void dijkstra(int start){
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    dist[start]=0;
+    pq.push({0,start});
+    while(!pq.empty()){
+        int now=pq.top().second;
+        int dis=pq.top().first;
         pq.pop();
-        
-        // 현 도시까지 비용이 이미 기록된 비용보다 크면 pass
-        if (vis[x] < cost) 
+        if(dist[now]<dis){
             continue;
-        
-        // x 도시와 이어진 도시들 검사
-        for (int i = 0; i < v[x].size(); i++){
-            int nx = v[x][i].first; // 다음 도시 
-            int ncost = cost+v[x][i].second; // 다음 도시까지 비용
- 
-            // 기록되어진 비용보다 지금 비용이 더 작다면
-            // 큐에 넣어줌 
-            if (vis[nx] > ncost){ 
-                pq.push(make_pair(ncost, nx));
-                vis[nx] = ncost; // 비용 다시 기록
+        }
+        for(int i=0;i<v[now].size();i++){
+            int next=v[now][i].first;
+            int nxcost=v[now][i].second+dis;
+            if(dist[next]>nxcost){
+                pq.push({nxcost,next});
+                dist[next]=nxcost;
             }
         }
     }
 }
+
 int main(){
-    int n, m;
-    cin >> n >> m;
-    
-    v[0].push_back(make_pair(0, 0));
-    memset(vis, 98765432, sizeof(vis)); // 모든 비용 최대로 
-    for (int i = 0; i < m; i++){
-        int a, b, c;
-        cin >> a >> b >> c;
-        v[a].push_back(make_pair(b, c));
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int cnt;
+    v[0].push_back({0,0});
+    fill(dist, dist + 1001, 2147483647);
+    cin>>N>>M;
+    for(int i=0;i<M;i++){
+        cin>>s>>e>>cnt;
+        v[s].push_back(make_pair(e,cnt));
     }
-    int st, dt; // 출발 도시, 도착 도시
-    cin >> st >> dt;
-    fc(st); 
-    cout << vis[dt]; // dt 도시의 비용 출력
+    cin>>s>>e;
+    dijkstra(s);
+    cout<<dist[e];
     return 0;
 }
