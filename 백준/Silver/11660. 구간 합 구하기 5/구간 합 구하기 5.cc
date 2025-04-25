45d2cea8c485e54435d4bd18fe3c95e1abd46f5c
ImHyungsuk <limhs8630@gmail.com>
2025-04-25 19:27:16 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 392 ms, Memory: 6508 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -11,14 +11,12 @@ int main(){
     for(int i=1;i<N+1;i++){
         for(int j=1;j<N+1;j++){
             cin>>input;
-            v[i][j]=v[i][j-1]+input;
+            v[i][j]=v[i][j-1]+v[i-1][j]+input-v[i-1][j-1];
         }
     }
     for(int i=0;i<M;i++){
         cin>>x1>>y1>>x2>>y2;
-        for(int x=x1;x<=x2;x++){
-            ret[i]+=v[x][y2]-v[x][y1-1];
-        }
+        ret[i]=v[x2][y2]-v[x1-1][y2]-v[x2][y1-1]+v[x1-1][y1-1];
     }
     for(int i=0;i<M;i++){
         cout<<ret[i]<<"\n";
