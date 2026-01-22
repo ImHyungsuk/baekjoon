25c5c1967652c0695ad9143e85b7232c88239a44
ImHyungsuk <limhs8630@gmail.com>
2026-01-22 15:28:34 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,26 +1,17 @@
 import java.util.*;
 import java.io.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
-        Map<String,Integer> map=new HashMap<>();
-        ArrayList<String> arr=new ArrayList<>();
-        for(int i=0;i<clothes.length;i++){
-            if(map.containsKey(clothes[i][1])){
-                int cnt=map.get(clothes[i][1]);
-                cnt++;
-                map.put(clothes[i][1],cnt);
-            }else{
-                map.put(clothes[i][1],1);
-                arr.add(clothes[i][1]);
-            }
+        int answer = 1;
+        HashMap<String,Integer>map=new HashMap<>();
+        for (int i=0;i<clothes.length;i++){
+            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
         }
-        int mul=1;
-        for(int i=0;i<arr.size();i++){
-            String key = arr.get(i);
-            mul*=(map.get(key)+1);
+        for(String str:map.keySet()){
+            answer*=(map.get(str)+1);
         }
-        answer=mul-1;
+        answer--;
         
         return answer;
     }
