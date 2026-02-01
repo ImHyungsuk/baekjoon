69d5f2a3c7ac9c2c30b84a716efb3457b585582a
ImHyungsuk <limhs8630@gmail.com>
2026-02-01 17:05:48 +0900

    [Gold II] Title: 1의 개수 세기, Time: 92 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.java
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.java
@@ -1,34 +1,33 @@
+// tip: each public class is put in its own file
 import java.util.*;
 import java.io.*;
-
 public class Main {
-	static long A, B;
-	static long[] dp;
+    // tip: arguments are passed via the field below this editor
+    static long a, b;
+    static long [] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        a = Long.parseLong(st.nextToken());
+        b = Long.parseLong(st.nextToken());
+        dp = new long[55];
+        dp[0] = 1;
+        for (int i = 1; i < 55; i++) {
+            dp[i] = dp[i-1]*2+(long)Math.pow(2,i);
+        }
+        long result=calOne(b)-calOne(a-1);
+        System.out.println(result);
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		A = Long.parseLong(st.nextToken());
-		B = Long.parseLong(st.nextToken());
-		dp = new long[55];
-		dp[0] = 1;
-		for (int i = 1; i < 55; i++) {
-			dp[i] = (dp[i - 1] << 1) + (1L << i);
-		}
-		long result = calOne(B) - calOne(A - 1);
-		System.out.println(result);
-	}
-
-	static long calOne(long N) {
-		long count = N & 1;
-		int size = (int)(Math.log(N) / Math.log(2));
-
-		for (int i = size; i > 0; i--) {
-			if ((N & (1L << i)) != 0L) {
-				count += dp[i - 1] + (N - (1L << i) + 1);
-				N -= (1L << i);
-			}
-		}
-		return count;
-	}
+    static long calOne(long n){
+        int size=(int)(Math.log(n)/Math.log(2));
+        long cnt=n&1;
+        for(int i=size;i>0;i--){
+            if((n&(1L<<i))!=0){
+                cnt+=dp[i-1]+(n-(1L<<i)+1);
+                n-=(1L<<i);
+            }
+        }
+        return cnt;
+    }
 }
