7f351eaf8696c1eae67b8884be8fab30bfa867ef
ImHyungsuk <limhs8630@gmail.com>
2026-03-13 17:04:30 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,18 +1,16 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        HashMap<String,Integer>map=new HashMap<>();
-        for (int i=0;i<clothes.length;i++){
-            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
+        Map<String,Integer>map=new HashMap<>();
+        for(String[]cloth:clothes){
+            map.put(cloth[1],map.getOrDefault(cloth[1],0)+1);
         }
-        for(String str:map.keySet()){
-            answer*=(map.get(str)+1);
+        for(String key:map.keySet()){
+            answer*=(map.get(key)+1);
         }
         answer--;
-        
         return answer;
     }
 }
