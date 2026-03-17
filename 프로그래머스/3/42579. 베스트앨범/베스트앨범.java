888e6d49981abef2e49e4c0f320201e6edcf39dd
ImHyungsuk <limhs8630@gmail.com>
2026-03-17 14:43:09 +0900

    [level 3] Title: 베스트앨범, Time: 2.28 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,73 +2,49 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
-        int len=genres.length;
-        HashMap<String,Integer>genrePlay =new HashMap<>(),count=new HashMap<>();
-        for(int i=0;i<len;i++){
-            genrePlay.put(genres[i],genrePlay.getOrDefault(genres[i],0)+plays[i]);
-            count.put(genres[i],count.getOrDefault(genres[i],0)+1);
+        int l=genres.length;
+        Map<String,List<Song>>count=new HashMap<>();
+        Map<String,Integer>genrePlay=new HashMap<>();
+        PriorityQueue<Genre>pq=new PriorityQueue<>((g1,g2)->g2.plays-g1.plays);
+        for(int i=0;i<l;i++){
+            if(!count.containsKey(genres[i])) {
+                count.put(genres[i],new ArrayList<>());
+                genrePlay.put(genres[i],0);
+            }
+            genrePlay.put(genres[i],genrePlay.get(genres[i])+plays[i]);
+            count.get(genres[i]).add(new Song(i,plays[i]));
         }
-        PriorityQueue<Genre>pq=new PriorityQueue<>();
         int c=0;
-        for(String str:genrePlay.keySet()){
-            pq.add(new Genre(str,genrePlay.get(str)));
-            c+=count.get(str)>2?2:count.get(str);
+        for(String key:genrePlay.keySet()){
+            pq.add(new Genre(key,genrePlay.get(key)));
+            c+=count.get(key).size()>=2?2:count.get(key).size();
         }
         answer=new int[c];
-        // System.out.println("길이: "+c);
         int idx=0;
         while(!pq.isEmpty()){
-            Genre cur=pq.poll();
-            PriorityQueue<Song>spq=new PriorityQueue<>();
-            int tmp=0;
-            for(int i=0;i<len;i++){
-                    // System.out.println("장르: "+cur.name+ " num: "+i+" genres[i]: "+genres[i]);
-                if(genres[i].equals(cur.name)){
-                    // System.out.println("투입 장르: "+cur.name+ " num: "+i+" genres[i]: "+genres[i]);
-                    spq.add(new Song(i,plays[i]));
-                }
-            }
-            
+            String g=pq.poll().name;
+            List<Song>sList=count.get(g);
+            sList.sort((s1,s2)->s2.plays-s1.plays);
             int cnt=0;
-            while(!spq.isEmpty()){
-                Song s=spq.poll();
-                if(cnt>=2) break;
-                answer[idx++]=s.num;
-                cnt++;
-            }
+            while(cnt<sList.size()&&cnt<2) answer[idx++]=sList.get(cnt++).num;
         }
-        
         return answer;
     }
 }
 
-class Genre implements Comparable<Genre>{
+class Genre{
     String name;
     int plays;
-    
-    public Genre(String name,int plays){
+    public Genre(String name, int plays){
         this.name=name;
         this.plays=plays;
     }
-    
-    @Override
-    public int compareTo(Genre o){
-        return o.plays-this.plays;
-    }
 }
 
-class Song implements Comparable<Song>{
+class Song{
     int num,plays;
     public Song(int num,int plays){
         this.num=num;
         this.plays=plays;
-    }
-    
-    @Override
-    public int compareTo(Song o){
-        if(o.plays==this.plays){
-            return this.num-o.num;
-        }
-        return o.plays-this.plays;
     }
 }
