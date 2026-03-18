d78d3592f35036ae51af07f3706413ab63b45c30
ImHyungsuk <limhs8630@gmail.com>
2026-03-18 12:31:41 +0900

    [level 2] Title: 올바른 괄호, Time: 18.41 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,22 @@
 import java.util.*;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
-        Stack<Character> st=new Stack<>();
-        for(int idx=0;idx<s.length();idx++){
-            if(s.charAt(idx)=='(')
-                st.push('(');
+        char[]arr=s.toCharArray();
+        Stack<Character>st=new Stack<>();
+        for(char c:arr){
+            if(c=='(')st.add(c);
             else {
                 if(st.isEmpty()){
                     answer=false;
-                    return answer;
+                    break;
                 }
                 st.pop();
             }
         }
-        answer=st.isEmpty();
+        if(!st.isEmpty())answer=false;
 
         return answer;
     }
