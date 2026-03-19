4ca6b3d2c3ee17b98c2185851a643b38becd896b
ImHyungsuk <limhs8630@gmail.com>
2026-03-19 15:21:35 +0900

    [level 2] Title: 프로세스, Time: 2.80 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -4,39 +4,29 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         PriorityQueue<Integer>pq=new PriorityQueue<>(Comparator.reverseOrder());
-        Queue<Integer> q=new ArrayDeque<>();
-        for(int i=0;i<priorities.length;i++){
+        Queue<Task>q=new ArrayDeque<>();
+        
+        int l=priorities.length;
+        for(int i=0;i<l;i++){
             pq.add(priorities[i]);
-            q.add(i);
+            q.add(new Task(i,priorities[i]));
         }
-        answer=0;
-        int v=pq.peek();
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            if(priorities[cur] == v){
-                pq.poll();
-                answer++;
-                if(cur == location) break;
-                v = pq.peek();
-            } else q.add(cur);
+        while(!pq.isEmpty()){
+            int cur_p=pq.poll();
+            while(q.peek().p!=cur_p){
+                q.add(q.poll());
+            }
+            answer++;
+            if(location==q.poll().num) break;
         }
         return answer;
     }
 }
 
-class Task implements Comparable<Task>{
-    int num,priority;
-    
-    public Task(int num,int priority){
+class Task{
+    int num,p;
+    public Task(int num,int p){
         this.num=num;
-        this.priority=priority;
-    }
-    
-    @Override
-    public int compareTo(Task o){
-        if(o.priority==this.priority){
-            return this.num-o.num;
-        }
-        return o.priority-this.priority;
+        this.p=p;
     }
 }
