e36ea439e88a627ddefcd2a4923cb0515124e111
ImHyungsuk <limhs8630@gmail.com>
2026-03-21 14:21:32 +0900

    [level 2] Title: 주식가격, Time: 15.12 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,36 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        PriorityQueue<Stock>pq=new PriorityQueue<>();
-        int t=1,idx=0;
-        pq.add(new Stock(prices[0],0));
-        while(t<prices.length){
-            while(!pq.isEmpty()&&prices[t]<pq.peek().price){
-                answer[pq.peek().time]=t-pq.poll().time;
+        int n=prices.length;
+        int[] answer = new int[n];
+        boolean[]isEnd=new boolean[n];
+        for(int i=0;i<n;i++){
+            int j=i+1;
+            while(j<n&&prices[i]<=prices[j]){
+                j++;
             }
-            pq.add(new Stock(prices[t],t));
-            t++;
+            if(j==n){
+                answer[i]=j-i-1;
+            }else{
+                answer[i]=j-i;
+            }
         }
-        t--;
-        while(!pq.isEmpty()){
-            answer[pq.peek().time]=t-pq.poll().time;
-        }
-        // answer[prices.length-1]--;
+        
         return answer;
-    }
-}
-
-class Stock implements Comparable<Stock>{
-    int price,time;
-    
-    public Stock(int price, int time){
-        this.price=price;
-        this.time=time;
-    }
-    
-    @Override public int compareTo(Stock o){
-        return o.price-this.price;
     }
 }
