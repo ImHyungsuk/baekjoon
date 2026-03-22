0ce3fead04cfb35fbb45a5c3be48003baf667d46
ImHyungsuk <limhs8630@gmail.com>
2026-03-22 17:33:11 +0900

    [level 2] Title: 더 맵게, Time: 1699.51 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,18 +1,19 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
         for(int s:scoville){
             pq.add(s);
         }
-        while(pq.size()>1&&pq.peek()<K){
+        while(pq.size()>=2 && pq.peek()<K){
             int a=pq.poll();
             int b=pq.poll();
-            pq.add(a+(2*b));
+            pq.add(a+(b*2));
             answer++;
         }
-        if(pq.peek()<K) answer=-1;
+        if(pq.peek()<K)answer=-1;
         return answer;
     }
 }
