170d4de75d7a196ba9bd8ceb17333d066a5d7f41
ImHyungsuk <limhs8630@gmail.com>
2026-03-27 15:52:28 +0900

    [level 2] Title: H-Index, Time: 0.91 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = citations.length;
+        int answer = 0;
         Arrays.sort(citations);
         for(int i=0;i<citations.length;i++){
-            if(citations[i]>=answer) break;
-            answer--;
+            int h=citations.length-i;
+            if(h<=citations[i]) return h;
         }
         return answer;
     }
