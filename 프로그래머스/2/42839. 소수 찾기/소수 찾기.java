3c52bec8272d27bb8557844c6f980559779824e1
ImHyungsuk <limhs8630@gmail.com>
2026-03-29 14:33:19 +0900

    [level 2] Title: 소수 찾기, Time: 110.64 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,39 +1,38 @@
 import java.util.*;
-
 class Solution {
     static Set<Integer>set=new HashSet<>();
-    static boolean[]visited;
     public int solution(String numbers) {
         int answer = 0;
-        visited=new boolean[numbers.length()];
-        dfs("",numbers);
+        boolean[]isNotPrime=new boolean[10000000];
+        isNotPrime[0]=true;
+        isNotPrime[1]=true;
+        for(int i=2;i<10000000;i++){
+            if(isNotPrime[i])continue;
+            for (int j=2;i*j<10000000;j++){
+                isNotPrime[i*j]=true;
+            }
+        }
+        int len=numbers.length();
+        char[]arr=numbers.toCharArray();
+        boolean[]visited=new boolean[len];
+        dfs(arr,visited,"",isNotPrime);
         answer=set.size();
         return answer;
     }
     
-    static void dfs(String current,String numbers){
+    static void dfs(char[]arr,boolean[]visited,String current,boolean[]isNotPrime){
+        int len=arr.length;
         if(!current.equals("")){
             int num=Integer.parseInt(current);
-            if(isPrime(num)){
-                // System.out.println(num);
+            if(!isNotPrime[num])
                 set.add(num);
-            }
         }
-        int len=numbers.length();
         for(int i=0;i<len;i++){
             if(!visited[i]){
                 visited[i]=true;
-                dfs(current+numbers.charAt(i),numbers);
+                dfs(arr,visited,current+arr[i],isNotPrime);
                 visited[i]=false;
             }
         }
-    }
-    
-    static boolean isPrime(int num){
-        if(num<2)return false;
-        for(int i=2;i*i<=num;i++){
-            if(num%i==0) return false;
-        }
-        return true;
     }
 }
