2ac988d25002eaab530cff011709a892e78ca2da
ImHyungsuk <limhs8630@gmail.com>
2026-04-01 11:18:38 +0900

    [level 2] Title: 피로도, Time: 2.92 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,31 @@
 class Solution {
-    static boolean[]visited;
     public int solution(int k, int[][] dungeons) {
         int answer = -1;
-        int len=dungeons.length;
-        visited=new boolean[len];
-        
-        for(int i=0;i<len;i++){
+        boolean[]visited=new boolean[dungeons.length];
+        for(int i=0;i<dungeons.length;i++){
+            int tmp=0;
             if(!visited[i]&&k>=dungeons[i][0]){
                 visited[i]=true;
-                answer=Math.max(answer,dfs(k-dungeons[i][1],len,dungeons));
+                tmp=dfs(k-dungeons[i][1],dungeons,visited);
                 visited[i]=false;
             }
+            answer=answer<tmp?tmp:answer;
         }
         return answer;
     }
     
-    static int dfs(int k,int len,int[][]dungeons){
+    static int dfs(int k, int[][] dungeons, boolean[]visited){
         int cnt=1;
-        for(int i=0;i<len;i++){
+        int max=0;
+        for(int i=0;i<dungeons.length;i++){
+            int tmp=0;
             if(!visited[i]&&k>=dungeons[i][0]){
                 visited[i]=true;
-                cnt=Math.max(cnt, dfs(k-dungeons[i][1],len,dungeons)+1);
+                tmp+=dfs(k-dungeons[i][1],dungeons,visited);
                 visited[i]=false;
             }
+            max=max<tmp?tmp:max;
         }
-        return cnt;
+        return cnt+max;
     }
 }
