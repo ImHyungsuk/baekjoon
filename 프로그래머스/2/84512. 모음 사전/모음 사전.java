a158ed9b9ce379dd0a75cc3653d0632f7c537344
ImHyungsuk <limhs8630@gmail.com>
2026-04-03 18:04:13 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,34 +1,25 @@
 import java.util.*;
-
 class Solution {
-    static int[] charToNum=new int['Z'];
+    static char[]arr=new char[]{'A','E','I','O','U'};
+    static Map<Character,Integer>map=Map.of(
+        'A',1,
+        'E',2,
+        'I',3,
+        'O',4,
+        'U',5
+    );
     public int solution(String word) {
         int answer = 0;
         int len=word.length();
-        charToNum['A']=0;
-        charToNum['E']=1;
-        charToNum['I']=2;
-        charToNum['O']=3;
-        charToNum['U']=4;
-        // AEEEE: 1+(1+1*5^(5-2))+1*5^(5-3)+1*5^(5-4)+1*5^(5-5)+1
-        // AAAAE: 1+1+1+1+2
-        // AAAE: 1+1+1+1+1*(1+5)
-        // I: 1+2*(1+5(1+5(1+5(1+5))))=1+2*781=1563
-        // EIO: 1*(1+5(1+5(1+5(1+5))))+1+2*(1+5(1+5(1+5)))+1+3*(1+5(1+5))+1=781+1+312+1+93+1=782+313+94=1095+94=1189
-        
-        answer=count(word,0,len);
-        return answer;
-    }
-    static int count(String word,int idx,int len){
-        if(idx==len) return 0;
-        int ret=1;
-        int a=charToNum[word.charAt(idx)];
-        // System.out.println(word.charAt(idx)+": "+a);
-        for(int i=idx+1;i<5;i++){
-            ret=ret*5+1;
+        for(int i=0;i<len;i++){
+            int idx=map.get(word.charAt(i));
+            int cnt=1;
+            for(int j=i;j<4;j++){
+                cnt=cnt*5+1;
+            }
+            answer+=cnt*(idx-1)+1;
         }
-        ret=ret*a+1;
-        ret+=count(word,idx+1,len);
-        return ret;
+        
+        return answer;
     }
 }
