8167a58cc3b51f83a234d039ae8f966f6d3a381f
ImHyungsuk <limhs8630@gmail.com>
2026-04-04 15:38:08 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 400 ms, Memory: 49896 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -1,90 +1,85 @@
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int n, m;
+	static ArrayList<Node>[] next;
+	static int[] routes;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		ArrayList<Node>[] map = new ArrayList[n + 1];
-		int[] dist = new int[n + 1];
-		int[] route = new int[n + 1];
-		boolean[] visited = new boolean[n + 1];
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		next = new ArrayList[n + 1];
 		for (int i = 1; i <= n; i++) {
-			map[i] = new ArrayList<Node>();
+			next[i] = new ArrayList<>();
 		}
-
 		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			map[start].add(new Node(end, cost));
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			next[s].add(new Node(e, w));
 		}
-		st = new StringTokenizer(br.readLine(), " ");
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
-
-		dijkstra(start, end, map, dist, route, visited);
-		System.out.println(dist[end]);
-		int idx = end;
-		ArrayList<Integer> routes = new ArrayList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int s = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		System.out.println(dijkstra(s, e));
+		ArrayList<Integer> route = new ArrayList<>();
+		int idx = e;
 		while (idx != 0) {
-			routes.add(idx);
-			idx = route[idx];
+			route.add(idx);
+			idx = routes[idx];
 		}
-		System.out.println(routes.size());
+		System.out.println(route.size());
 		StringBuilder sb = new StringBuilder();
-		for (int i = routes.size() - 1; i >= 0; i--) {
-			sb.append(routes.get(i)).append(" ");
+		for (int i = route.size() - 1; i >= 0; i--) {
+			sb.append(route.get(i)).append(" ");
 		}
 		System.out.println(sb);
 	}
 
-	static void dijkstra(int start, int end, ArrayList<Node>[] map, int[] dist, int[] route, boolean[] visited) {
-		Arrays.fill(dist, 100000001);
-		dist[0] = 0;
-		route[start] = 0;
+	static int dijkstra(int s, int e) {
+		routes = new int[n + 1];
+		int[] dist = new int[n + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[s] = 0;
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(start, 0));
+		pq.add(new Node(s, 0));
 		while (!pq.isEmpty()) {
 			Node cur = pq.poll();
-			int cur_index = cur.index;
-			int cur_cost = cur.cost;
-			if (dist[cur_index] < cur_cost) {
+			int curNum = cur.num;
+			int curWeight = cur.weight;
+			if (dist[curNum] < curWeight)
 				continue;
-			}
-			if (visited[cur_index]) {
-				continue;
-			}
-			visited[cur_index] = true;
-			for (int i = 0; i < map[cur_index].size(); i++) {
-				Node next = map[cur_index].get(i);
-				int next_cost = cur_cost + next.cost;
-				if (!visited[next.index] && dist[next.index] > next_cost) {
-					dist[next.index] = next_cost;
-					pq.add(new Node(next.index, next_cost));
-					route[next.index] = cur_index;
+			if (curNum == e)
+				return curWeight;
+			for (Node nxt : next[curNum]) {
+				if (dist[nxt.num] > dist[curNum] + nxt.weight) {
+					dist[nxt.num] = dist[curNum] + nxt.weight;
+					pq.add(new Node(nxt.num, dist[nxt.num]));
+					routes[nxt.num] = curNum;
 				}
 			}
 		}
+		return dist[e];
 	}
 }
+class Node implements Comparable<Node>{
+	int num, weight;
 
-class Node implements Comparable<Node> {
-	int index;
-	int cost;
-
-	Node(int index, int cost) {
-		this.index = index;
-		this.cost = cost;
+	public Node(int num, int weight) {
+		this.num = num;
+		this.weight = weight;
 	}
 
 	@Override
-	public int compareTo(Node o) {
-		return this.cost - o.cost;
+	public int compareTo(Node node) {
+		return this.weight - node.weight;
 	}
 }
