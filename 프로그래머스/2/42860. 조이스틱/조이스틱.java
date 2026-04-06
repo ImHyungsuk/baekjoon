f69de38bc951d77d9c99d8ac1131a52d8e4b31e7
ImHyungsuk <limhs8630@gmail.com>
2026-04-06 18:56:17 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -2,27 +2,23 @@ class Solution {
     public int solution(String name) {
         int answer = 0;
         int len=name.length();
-        int[]count=new int[len];
-        int[]parents=new int[len];
+        int max=('Z'-'A');
+        int []count=new int[len];
         for(int i=0;i<len;i++){
-            parents[i]=i;
             char c=name.charAt(i);
-            if((c-'A')>('Z'-'A')/2) count[i]='Z'-c+1;
-            else count[i]=c-'A';
+            int cnt=c-'A';
+            cnt=cnt>max/2?'Z'-c+1:cnt;
+            count[i]=cnt;
             answer+=count[i];
         }
         int move=len-1;
-        for(int i=0;i<len;i++){
-            int next=i+1;
-            while(next<len&&count[next]==0){
-                next++;
-            }
-            move=Math.min(move,2*i+(len-next));
-            move=Math.min(move,i+2*(len-next));
+        for(int i=1;i<len;i++){
+            int tmp=i-1;
+            while(i<len&&count[i]==0)i++;
+            move=Math.min(move,2*tmp+len-i);
+            move=Math.min(move,tmp+2*(len-i));
         }
         answer+=move;
-        
-        
         return answer;
     }
 }
