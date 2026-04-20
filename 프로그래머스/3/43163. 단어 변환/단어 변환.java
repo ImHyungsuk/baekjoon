a7ecf72775239f4d527e54368c767cab541cc755
ImHyungsuk <limhs8630@gmail.com>
2026-04-20 11:15:05 +0900

    [level 3] Title: 단어 변환, Time: 0.76 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,43 +1,60 @@
 import java.util.*;
-
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = Integer.MAX_VALUE-1;
-        int len=target.length();
-        int words_len=words.length;
-        boolean[]visited=new boolean[words_len];
-        char[]arr=new char[len];
-        Queue<String>q=new ArrayDeque<>();
-        q.add(begin);
-        int level=0;
-        while(!q.isEmpty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                String cur=q.poll();
-                if(cur.equals(target)) answer=Math.min(level,answer);
-                for(int j=0;j<words_len;j++){
-                    int idx;
-                    if(!visited[j]&&canChange(cur,words[j])){
-                        q.add(words[j]);
-                        visited[j]=true;
-                    }
-                }
+        int answer = 0;
+        
+        boolean flag=false;
+        for(int i=0;i<words.length;i++){
+            if(target.equals(words[i])){
+                flag=true;
+                break;
             }
-            level++;
         }
-        return (answer==Integer.MAX_VALUE-1)?0:answer;
+        if(!flag) return answer;
+        answer=bfs(begin,target,words);
+        
+        return answer;
     }
     
-    static boolean canChange(String cur, String next){
-        int len=cur.length();
-        int cnt=0;
-        int idx=-1;
-        for(int i=0;i<len;i++){
-            if(cur.charAt(i)!=next.charAt(i)) {
-                cnt++;
+    public int bfs(String begin, String target, String []words){
+        int len=words.length;
+        int cnt=len;
+        boolean[]visited=new boolean[len];
+        Queue<Word>q=new LinkedList<>();
+        q.add(new Word(begin,0));
+        while(!q.isEmpty()){
+            Word cur=q.poll();
+            if(cur.word.equals(target)){
+                cnt=Math.min(cnt,cur.cost);
+            }
+            for(int i=0;i<len;i++){
+                if(visited[i])continue;
+                if(getDiff(cur.word,words[i])){
+                    visited[i]=true;
+                    q.add(new Word(words[i],cur.cost+1));
+                }
             }
         }
-        if(cnt==1)return true;
-        else return false;
+        return cnt;
+    }
+    
+    public boolean getDiff(String current, String next){
+        int len=current.length();
+        int cnt=0;
+        if(len!=next.length())return false;
+        for(int i=0;i<len;i++){
+            if(current.charAt(i)!=next.charAt(i))cnt++;
+            if(cnt>1)return false;
+        }
+        return true;
+    }
+}
+
+class Word{
+    String word;
+    int cost;
+    public Word(String word,int cost){
+        this.word=word;
+        this.cost=cost;
     }
 }
