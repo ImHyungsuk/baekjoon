638cedac8a2ea534e1e0e176daa51815c75f234e
ImHyungsuk <limhs8630@gmail.com>
2026-04-21 11:39:39 +0900

    [level 3] Title: 아이템 줍기, Time: 1.24 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,68 +1,57 @@
 import java.util.*;
 class Solution {
-    static int [][]board=new int[102][102];
-    static boolean [][]visited=new boolean[102][102];
-    static int[]dx={-1,1,0,0};
-    static int[]dy={0,0,-1,1};
+    static int MAX=102;
+    static int[]dx={1,-1,0,0};
+    static int[]dy={0,0,1,-1};
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
-        for(int []r:rectangle){
-            int x1=r[0]*2;
-            int y1=r[1]*2;
-            int x2=r[2]*2;
-            int y2=r[3]*2;
-            
-            for(int i=x1;i<=x2;i++){
-                for(int j=y1;j<=y2;j++){
-                    board[j][i]=1;
-                }
-            }
-        }
-        
+        int [][]board=new int[MAX][MAX];
         for(int[]r:rectangle){
             int x1=r[0]*2;
             int y1=r[1]*2;
             int x2=r[2]*2;
             int y2=r[3]*2;
-            
-            for(int i=x1+1;i<x2;i++){
-                for(int j=y1+1;j<y2;j++){
-                    board[j][i]=0;
+            for(int x=x1;x<=x2;x++){
+                for(int y=y1;y<=y2;y++){
+                    board[y][x]=1;
                 }
             }
         }
-		// StringBuilder sb = new StringBuilder();
-		// for (int i = 0; i < 10; i++) {
-		// 	for (int j = 0; j < 10; j++) {
-		// 		sb.append(board[j][i]).append(" ");
-		// 	}
-		// 	sb.append("\n");
-		// }
-		// System.out.println(sb);
-        answer=bfs(characterX*2, characterY*2, itemX*2, itemY*2);
+        for(int[]r:rectangle){
+            int x1=r[0]*2;
+            int y1=r[1]*2;
+            int x2=r[2]*2;
+            int y2=r[3]*2;
+            for(int x=x1+1;x<x2;x++){
+                for(int y=y1+1;y<y2;y++){
+                    board[y][x]=0;
+                }
+            }
+        }
+        answer=bfs(characterX*2,characterY*2,itemX*2,itemY*2,board);
         return answer;
     }
-    static int bfs(int sx,int sy,int tx,int ty){
-        Queue<int[]>q=new ArrayDeque<>();
-        q.add(new int[]{sx,sy,0});
-        visited[sy][sx]=true;
+    
+    static int bfs(int cX,int cY,int iX,int iY,int[][]board){
+        int ret=0;
+        boolean[][]visited=new boolean[MAX][MAX];
+        Queue<int[]>q=new LinkedList<>();
+        q.add(new int[]{cX,cY,0});
+        visited[cY][cX]=true;
         while(!q.isEmpty()){
             int[]cur=q.poll();
-            int x=cur[0];
-            int y=cur[1];
-            int dist=cur[2];
-            
-            if(x==tx&&y==ty){
-                return dist/2;
-            }
+            if(cur[0]==iX&&cur[1]==iY)
+                return cur[2]/2;
             for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
+                int nX=cur[0]+dx[i];
+                int nY=cur[1]+dy[i];
                 
-                if(nx<0||nx>=102||ny<0||ny>=102) continue;
-                if(!visited[ny][nx] && board[ny][nx]==1){
-                    visited[ny][nx]=true;
-                    q.add(new int[]{nx,ny,dist+1});
+                if(nX<0||nX>=102||nY<0||nY>=102||visited[nY][nX])
+                    continue;
+                
+                if(board[nY][nX]==1){
+                    q.add(new int[]{nX,nY,cur[2]+1});
+                    visited[nY][nX]=true;
                 }
             }
         }
