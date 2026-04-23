2204eabf9731ee9258526369f1b2cde4be0649b2
ImHyungsuk <limhs8630@gmail.com>
2026-04-23 13:52:41 +0900

    [level 3] Title: 여행경로, Time: 0.75 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,32 +1,36 @@
 import java.util.*;
 class Solution {
-    static ArrayList<String>arr=new ArrayList<>();
-    static boolean[]visited;
     public String[] solution(String[][] tickets) {
         String[] answer = {};
-        Arrays.sort(tickets,(t1,t2)->{
-            if(t1[0].equals(t2[0])) return t1[1].compareTo(t2[1]);
-            else return t1[0].compareTo(t2[0]);
-        });
-        visited=new boolean[tickets.length];
-        dfs("ICN","ICN",tickets,0);
-        answer=arr.get(0).split(" ");
+        Arrays.sort(tickets,(String[]s1,String[]s2)->s1[1].compareTo(s2[1]));
+        Set<String>set=new HashSet<>();
+        for(int i=0;i<tickets.length;i++){
+            set.add(tickets[i][0]);
+            set.add(tickets[i][1]);
+        }
+        boolean[]visited=new boolean[tickets.length];
+        answer=new String[tickets.length+1];
+        answer[0]="ICN";
+        answer=dfs(tickets,answer,visited,0);
+        
         return answer;
     }
     
-    static void dfs(String current,String path,String[][]tickets,int count){
-        if(count==tickets.length){
-            arr.add(path);
-            return;
-        }
-        if(!arr.isEmpty())return;
+    static String[]dfs(String[][]tickets,String[]path,boolean[]visited,int idx){
+        String cur=path[idx];
+        if(idx==tickets.length)return path;
         for(int i=0;i<tickets.length;i++){
-            if(visited[i]) continue;
-            if(tickets[i][0].equals(current)){
+            if(visited[i])continue;
+            if(tickets[i][0].equals(cur)){
+                path[idx+1]=tickets[i][1];
                 visited[i]=true;
-                dfs(tickets[i][1],path+" "+tickets[i][1],tickets,count+1);
+                
+                String[]ret=dfs(tickets,path,visited,idx+1);
+                if(ret!=null)return ret;
+                
                 visited[i]=false;
             }
         }
+        return null;
     }
 }
