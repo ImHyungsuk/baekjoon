e9865807e62fb9cf0cd6f027b2971dbe6da45c39
ImHyungsuk <limhs8630@gmail.com>
2026-04-24 12:21:47 +0900

    [level 3] Title: 등굣길, Time: 2.16 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,26 +1,27 @@
+import java.util.*;
 class Solution {
-    static int div=1_000_000_007;
+    static int[][] map;
+    static long[][]dp;
+    static long div=1_000_000_007;
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        long[][]dp=new long[n][m];
-        boolean[][]water=new boolean[n][m];
-        for(int i=0;i<puddles.length;i++){
-            water[puddles[i][1]-1][puddles[i][0]-1]=true;
-        }
-        dp[0][0]=1;
+        map=new int[n][m];
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(water[i][j]){
-                    dp[i][j]=0;
-                    continue;
-                }
-                if(i>0)
-                    dp[i][j]=(dp[i][j]+dp[i-1][j])%div;
-                if(j>0)
-                    dp[i][j]=(dp[i][j]+dp[i][j-1])%div;
-            }
+            Arrays.fill(map[i],1);
         }
-        answer=(int)dp[n-1][m-1];
+        for(int[]p:puddles){
+            map[p[1]-1][p[0]-1]=0;
+        }
+        dp=new long[n][m];
+        answer=(int)DP(0,0,m,n);
         return answer;
+    }
+    
+    private long DP(int x,int y,int m,int n){
+        if(x==m-1&&y==n-1) return 1;
+        if(x>=m||y>=n)return 0;
+        if(map[y][x]==0) return 0;
+        if(dp[y][x]!=0)return dp[y][x];
+        return dp[y][x]=(DP(x+1,y,m,n)+DP(x,y+1,m,n))%div;
     }
 }
