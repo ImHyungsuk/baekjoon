1374331a1b17cd03a746a1d00b8eb3ba721dbf91
ImHyungsuk <limhs8630@gmail.com>
2026-06-12 21:03:47 +0900

    [level 3] Title: 네트워크, Time: 0.65 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,30 +2,23 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        answer=bfs(n,computers);
-        return answer;
-    }
-    
-    static int bfs(int n,int[][]map){
-        int cnt=0;
         boolean[]visited=new boolean[n];
-        Queue<Integer>q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(visited[i])continue;
-            q.add(i);
-            visited[i]=true;
-            cnt++;
+            Queue<Integer>q=new LinkedList<>();
+            if(!visited[i]){
+                visited[i]=true;
+                q.add(i);
+                answer++;
+            }
             while(!q.isEmpty()){
                 int cur=q.poll();
-                // System.out.println(cur);
                 for(int nxt=0;nxt<n;nxt++){
-                    if(cur==nxt || map[cur][nxt]!=1) continue;
-                    if(visited[nxt]) continue;
+                    if(cur==nxt||computers[cur][nxt]==0||visited[nxt])continue;
                     q.add(nxt);
                     visited[nxt]=true;
                 }
             }
         }
-        return cnt;
+        return answer;
     }
 }
