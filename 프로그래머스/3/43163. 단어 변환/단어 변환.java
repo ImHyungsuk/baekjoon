a902c0c4b5a37af0d9fc81a9eba3932db477f68b
ImHyungsuk <limhs8630@gmail.com>
2026-06-23 15:01:42 +0900

    [level 3] Title: 단어 변환, Time: 0.81 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,49 +1,31 @@
 import java.util.*;
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        
-        boolean flag=false;
-        for(int i=0;i<words.length;i++){
-            if(target.equals(words[i])){
-                flag=true;
-                break;
+        Queue<Word>q=new LinkedList<>();
+        boolean[] visited=new boolean[words.length];
+        q.add(new Word(0,begin));
+        while(!q.isEmpty()){
+            Word cur=q.poll();
+            if(cur.str.equals(target))return cur.num;
+            for(int i=0;i<words.length;i++){
+                if(visited[i])continue;
+                if(changeAvailable(cur.str,words[i])){
+                    visited[i]=true;
+                    q.add(new Word(cur.num+1,words[i]));
+                }
             }
+            
         }
-        if(!flag) return answer;
-        answer=bfs(begin,target,words);
-        
         return answer;
     }
     
-    public int bfs(String begin, String target, String []words){
-        int len=words.length;
-        int cnt=len;
-        boolean[]visited=new boolean[len];
-        Queue<Word>q=new LinkedList<>();
-        q.add(new Word(begin,0));
-        while(!q.isEmpty()){
-            Word cur=q.poll();
-            if(cur.word.equals(target)){
-                cnt=Math.min(cnt,cur.cost);
-            }
-            for(int i=0;i<len;i++){
-                if(visited[i])continue;
-                if(getDiff(cur.word,words[i])){
-                    visited[i]=true;
-                    q.add(new Word(words[i],cur.cost+1));
-                }
-            }
-        }
-        return cnt;
-    }
-    
-    public boolean getDiff(String current, String next){
-        int len=current.length();
+    private boolean changeAvailable(String cur, String next){
+        int len=cur.length();
         int cnt=0;
-        if(len!=next.length())return false;
         for(int i=0;i<len;i++){
-            if(current.charAt(i)!=next.charAt(i))cnt++;
+            if(cur.charAt(i)!=next.charAt(i)) cnt++;
             if(cnt>1)return false;
         }
         return true;
@@ -51,10 +33,10 @@ class Solution {
 }
 
 class Word{
-    String word;
-    int cost;
-    public Word(String word,int cost){
-        this.word=word;
-        this.cost=cost;
+    int num;
+    String str;
+    Word(int num, String str){
+        this.num=num;
+        this.str=str;
     }
 }
