cb512bad00449064a1d3c35fdb8739a7e64ca3dd
ImHyungsuk <limhs8630@gmail.com>
2026-06-29 20:52:05 +0900

    [level 3] Title: 가장 먼 노드, Time: 14.27 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,7 +1,9 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
+        int max=0;
         
         ArrayList<Integer>[]adj=new ArrayList[n+1];
         for(int i=1;i<=n;i++){
@@ -11,35 +13,34 @@ class Solution {
             adj[edge[i][0]].add(edge[i][1]);
             adj[edge[i][1]].add(edge[i][0]);
         }
-        
-        boolean[]visited=new boolean[n+1];
+        boolean []visited=new boolean[n+1];
         Queue<Node>q=new LinkedList<>();
+        
         q.add(new Node(1,0));
         visited[1]=true;
-        int max=0;
         while(!q.isEmpty()){
             Node cur=q.poll();
-            if(max==cur.cost) answer++;
-            else if(max<cur.cost){
+            
+            if(cur.cost>max){
                 answer=1;
                 max=cur.cost;
             }
+            else if(cur.cost==max)answer++;
             
             for(int nxt:adj[cur.num]){
                 if(!visited[nxt]){
-                    visited[nxt]=true;
-                    q.add(new Node(nxt,cur.cost+1));
+                visited[nxt]=true;
+                q.add(new Node(nxt,cur.cost+1));
                 }
             }
         }
-        
-        
         return answer;
     }
 }
+
 class Node{
     int num,cost;
-    public Node(int num, int cost){
+    Node(int num,int cost){
         this.num=num;
         this.cost=cost;
     }
