846ec5a7985f60f662fed6df4a4555a033d7a424
ImHyungsuk <limhs8630@gmail.com>
2026-06-30 14:50:35 +0900

    [level 3] Title: 단속카메라, Time: 4.24 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,12 +2,16 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
-        int loc=-30001;
-        Arrays.sort(routes,(r1,r2)->r1[1]-r2[1]);
-        for(int i=0;i<routes.length;i++){
-            if(routes[i][0]>loc){
+        Arrays.sort(routes, (r1,r2)->{
+           if(r1[1]==r2[1])return r1[0]-r2[0];
+            return r1[1]-r2[1];
+        });
+        int min=routes[0][1];
+        answer++;
+        for(int i=1;i<routes.length;i++){
+            if(routes[i][0]>min){
                 answer++;
-                loc=routes[i][1];
+                min=routes[i][1];
             }
         }
         return answer;
