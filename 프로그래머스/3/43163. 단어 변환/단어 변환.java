b82b97813ec5119e59f8fb2ea9d2cab3f79a6401
ImHyungsuk <limhs8630@gmail.com>
2026-07-20 22:09:24 +0900

    [level 3] Title: 단어 변환, Time: 1.20 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,36 +1,47 @@
+import java.util.*;
+
 class Solution {
-    static int answer=Integer.MAX_VALUE;
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
         boolean[]visited=new boolean[words.length];
-        dfs(begin,target,words,visited,0);
-        answer=answer==Integer.MAX_VALUE?0:answer;
+        Queue<Word>q=new LinkedList<>();
+        q.add(new Word(begin,0));
+        while(!q.isEmpty()){
+            Word cur=q.poll();
+            if(cur.word.equals(target)){
+                answer=cur.cnt;
+                break;
+            }
+            for(int i=0;i<words.length;i++){
+                if(visited[i])continue;
+                if(available(cur.word,words[i])){
+                    visited[i]=true;
+                    q.add(new Word(words[i],cur.cnt+1));
+                }
+            }
+        }
         return answer;
     }
     
-    void dfs(String cur, String target, String[] words,boolean[]visited,int cost){
-        if(cur.equals(target)){
-            answer=Math.min(answer,cost);
-        }
-        for(int i=0;i<words.length;i++){
-            if(visited[i])continue;
-            if(available(cur,words[i],target)){
-                visited[i]=true;
-                dfs(words[i],target,words,visited,cost+1);
-                visited[i]=false;
-            }
-        }
-    }
     
-    boolean available(String from, String to,String target){
-        int l=from.length();
-        if(l!=to.length())return false;
+    
+    boolean available(String cur, String nxt){
+        if(cur.length()!=nxt.length())return false;
         int cnt=0;
-        for(int i=0;i<l;i++){
-            if(from.charAt(i)!=to.charAt(i)){
-                cnt++;
-                if(cnt>1) return false;
-            }
+        for(int i=0;i<cur.length();i++){
+            if(cur.charAt(i)!=nxt.charAt(i))cnt++;
+            if(cnt>=2)return false;
         }
         return true;
+    }
+}
+
+class Word{
+    String word;
+    int cnt;
+    
+    Word(String word, int cnt){
+        this.word=word;
+        this.cnt=cnt;
     }
 }
